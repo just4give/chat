7704bf7029a881143d6ae76d97ed00adb458d2f2
unknown <Mithun.Das@M-DAS.dt.inc>
2016-02-22 16:33:32 -0500

    initial commit

---
+++ client/modules/app/AppModule.js
@@ -0,0 +1,65 @@
+var app = angular.module("chatApp",['btford.socket-io']);
+
+
+app.factory('Socket',[ "socketFactory",function (socketFactory) {
+    var myIoSocket = io.connect();
+
+    var socket = socketFactory({
+        ioSocket: myIoSocket
+    });
+
+    return socket;
+}]);
+
+
+app.controller('ChatController', ["$rootScope", "$scope","$log", "Socket", function($rootScope,$scope, $log,Socket){
+    $log.debug('ChatController initialized');
+
+    $scope.messages = [];
+
+    $scope.message ="";
+    $scope.room="";
+
+   $scope.send = function(message){
+       var newMsg = {
+           text: message,
+           room: $scope.room,
+           user:$scope.user,
+           self:true
+       }
+
+       $scope.messages.push(newMsg);
+       $scope.message="";
+       Socket.emit('send:message',newMsg);
+    }
+
+    $scope.join = function(room){
+        $scope.joined=true;
+        Socket.emit('user:joined',{
+            user: $scope.user,
+            room: $scope.room
+        });
+       $scope.messages.push(  {text:"Welcome, "+$scope.user+"!",room:$scope.room,self:false});
+
+    }
+
+    $scope.disconnect = function(){
+        Socket.emit('user:left',{
+            user: $scope.user,
+            room: $scope.room
+        });
+        $scope.joined=false;
+    }
+    Socket.on("receive:message",function(msg){
+         $scope.$apply(function(){
+            $scope.messages.push(msg);
+        });
+    });
+
+    Socket.on("setup",function(data){
+        $scope.$apply(function(){
+            $scope.rooms = data.rooms;
+        });
+    });
+
+}]);
---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;*/
+
+module.exports = function(io){
+
+  io.on( "connection", function( socket )
+  {
+    console.log( "A user connected" );
+    var defaultRoom = 'general';
+    var rooms = ["general", "angular", "socket.io", "express", "node", "mongo", "PHP", "laravel"];
+
+    socket.emit('setup', {
+      rooms: rooms
+    });
+
+    socket.on( "send:message", function( msg )
+    {
+      console.log("user message ",msg);
+      msg.self=false;
+      socket.in(msg.room).broadcast.emit("receive:message",msg);
+    });
+
+    socket.on( "user:joined", function( msg )
+    {
+
+      socket.join(msg.room);
+      socket.in(msg.room).broadcast.emit("receive:message",{ text:msg.user+' joined!', room: msg.room, self:false });
+    });
+
+    socket.on( "user:left", function( msg )
+    {
+      socket.leave(msg.room);
+      socket.in(msg.room).broadcast.emit("receive:message",{ text:msg.user+' left!', room: msg.room, self:false });
+    });
+
+  });
+
+
+
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Chat Application' });
+  });
+
+  return router;
+
+}
